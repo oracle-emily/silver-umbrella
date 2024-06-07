c8e0689a1bf3a4ac5fa7f0678fb7c2ce268c8c46
oracle-emily <128946320+oracle-emily@users.noreply.github.com>
2024-06-07 20:53:20 +0800

    顺序表

---
+++ csdn text6.7/csdn text6.7/SeqList.h
@@ -0,0 +1,32 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+
+typedef  int SLdataType;
+typedef  struct SeqList
+{
+	SLdataType* arr;//指向数组的指针
+	int size;//当前的长度
+	int capacity;//最大容量
+}SL;
+
+
+void SLInit(SL* psl);
+
+void SLdestory(SL* psl);
+
+void SLcheck(SL* psl);
+
+void SLPushBack(SL* ps, SLdataType x);
+
+void SLPushFront(SL* psl, SLdataType x);
+
+void SLPrint(SL s);
+
+void SLPopBack(SL* psl);//尾删
+
+void SLPopFront(SL* psl);//头删
+
+int SLFind(SL* psl, SLdataType x);
