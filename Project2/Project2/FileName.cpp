fc940cc868e8e1873b5b6fe20b4546c97fa843ce
oracle-emily <128946320+oracle-emily@users.noreply.github.com>
2024-10-09 23:38:32 +0800

    csdn示例

---
+++ Project2/Project2/FileName.cpp
@@ -0,0 +1,26 @@
+﻿class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	// 拷贝构造函数的正确写法  
+	Date(const Date& d) {
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+int main()
+{
+	Date d1;
+	Date d2(d1);
+	return 0;
+}
